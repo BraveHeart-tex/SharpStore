bbda33c8b3fe4d3ee5dadf1b210dd538f40d9702
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2022-08-26 11:13:17 +0300

    fixed basket page bugs and added checkout page.

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -2,6 +2,8 @@ import { Add, Delete, Remove } from '@mui/icons-material';
 import { LoadingButton } from '@mui/lab';
 import {
   Box,
+  Button,
+  Grid,
   Paper,
   Table,
   TableBody,
@@ -14,97 +16,160 @@ import {
 import { useState } from 'react';
 import agent from '../../app/api/agent';
 import { useStoreContext } from '../../app/context/StoreContext';
+import BasketSummary from './BasketSummary';
+import { Link } from 'react-router-dom';
+import { Container } from '@mui/system';
 
 export default function BasketPage() {
   const { basket, setBasket, removeItem } = useStoreContext();
-  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState({
+    loading: false,
+    name: '',
+  });
 
-  const handleAddItem = (productId: number) => {
-    setLoading(true);
+  const basketTotal = basket?.items.reduce(
+    (total, item) => total + item.quantity * item.price,
+    0
+  );
+
+  const handleAddItem = (productId: number, name: string) => {
+    setStatus({ loading: true, name });
     agent.Basket.addItem(productId)
       .then((basket) => setBasket(basket))
       .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
+      .finally(() => setStatus({ loading: false, name: '' }));
   };
 
-  const handleRemoveItem = (productId: number, quantity = 1) => {
-    setLoading(true);
+  const handleRemoveItem = (productId: number, quantity = 1, name: string) => {
+    setStatus({ loading: true, name });
     agent.Basket.removeItem(productId, quantity)
       .then(() => removeItem(productId, quantity))
       .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
+      .finally(() => setStatus({ loading: false, name: '' }));
   };
 
-  if (!basket) {
-    return <Typography variant='h3'>Your basket is empty</Typography>;
+  if (!basket || basket.items.length === 0) {
+    return (
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '80vh',
+        }}
+      >
+        <Typography variant='h3'>Your basket is empty</Typography>
+        <Button component={Link} to='/catalog'>
+          Go back to the catalog
+        </Button>
+      </Container>
+    );
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Product</TableCell>
-            <TableCell align='right'>Price</TableCell>
-            <TableCell align='center'>Quantity</TableCell>
-            <TableCell align='right'>Subtotal</TableCell>
-            <TableCell align='right'></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {basket.items.map((item) => (
-            <TableRow
-              key={item.productId}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component='th' scope='row'>
-                <Box display='flex' alignItems='center'>
-                  <img
-                    src={item.pictureUrl}
-                    alt={item.name}
-                    style={{ height: 50, marginRight: 20 }}
-                  />
-                  <span>{item.name}</span>
-                </Box>
-              </TableCell>
-              <TableCell align='right'>
-                ${(item.price / 100).toFixed(2)}
-              </TableCell>
-              <TableCell align='center'>
-                <LoadingButton
-                  loading={loading}
-                  color='error'
-                  onClick={() => handleRemoveItem(item.productId)}
-                >
-                  <Remove />
-                </LoadingButton>
-                {item.quantity}
-                <LoadingButton
-                  loading={loading}
-                  color='secondary'
-                  onClick={() => handleAddItem(item.productId)}
-                >
-                  <Add />
-                </LoadingButton>
-              </TableCell>
-              <TableCell align='right'>
-                ${((item.price * item.quantity) / 100).toFixed(2)}
-              </TableCell>
-              <TableCell align='right'>
-                <LoadingButton
-                  color='error'
-                  loading={loading}
-                  onClick={() =>
-                    handleRemoveItem(item.productId, item.quantity)
-                  }
-                >
-                  <Delete />
-                </LoadingButton>
-              </TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Product</TableCell>
+              <TableCell align='right'>Price</TableCell>
+              <TableCell align='center'>Quantity</TableCell>
+              <TableCell align='right'>Subtotal</TableCell>
+              <TableCell align='right'></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {basket.items.map((item) => (
+              <TableRow
+                key={item.productId}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component='th' scope='row'>
+                  <Box display='flex' alignItems='center'>
+                    <img
+                      src={item.pictureUrl}
+                      alt={item.name}
+                      style={{ height: 50, marginRight: 20 }}
+                    />
+                    <span>{item.name}</span>
+                  </Box>
+                </TableCell>
+                <TableCell align='right'>
+                  ${(item.price / 100).toFixed(2)}
+                </TableCell>
+                <TableCell align='center'>
+                  <LoadingButton
+                    loading={
+                      status.loading && status.name === 'rem' + item.productId
+                    }
+                    color='error'
+                    onClick={() =>
+                      handleRemoveItem(
+                        item.productId,
+                        1,
+                        'rem' + item.productId
+                      )
+                    }
+                  >
+                    <Remove />
+                  </LoadingButton>
+                  {item.quantity}
+                  <LoadingButton
+                    loading={
+                      status.loading && status.name === 'add' + item.productId
+                    }
+                    color='secondary'
+                    onClick={() =>
+                      handleAddItem(item.productId, 'add' + item.productId)
+                    }
+                  >
+                    <Add />
+                  </LoadingButton>
+                </TableCell>
+                <TableCell align='right'>
+                  ${((item.price * item.quantity) / 100).toFixed(2)}
+                </TableCell>
+                <TableCell align='right'>
+                  <LoadingButton
+                    color='error'
+                    loading={
+                      status.loading && status.name === 'del' + item.productId
+                    }
+                    onClick={() =>
+                      handleRemoveItem(
+                        item.productId,
+                        item.quantity,
+                        'del' + item.productId
+                      )
+                    }
+                  >
+                    <Delete />
+                  </LoadingButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {basket.items.length > 0 && (
+        <Grid container>
+          <Grid item xs={6} />
+          <Grid item xs={6}>
+            <BasketSummary basketTotal={basketTotal} />
+            <Button
+              component={Link}
+              to='/checkout'
+              variant='contained'
+              size='large'
+              fullWidth
+            >
+              Checkout
+            </Button>
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 }
