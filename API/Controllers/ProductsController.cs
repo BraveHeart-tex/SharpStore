5d783b862e9a98a436ae1255055aa7c8e55b3192
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2022-08-29 20:06:47 +0300

    added pagination,sorting,filtering to the API.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,9 +21,21 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
+    public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] ProductParams productParams)
     {
-      return await _context.Products.ToListAsync();
+      var query = _context.Products
+        .Sort(productParams.OrderBy)
+        .Search(productParams.SearchTerm)
+        .Filter(productParams.Brands, productParams.Types)
+        .AsQueryable();
+
+      var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+      Response.AddPaginationHeader(products.MetaData);
+
+
+      return products;
+
     }
 
     [HttpGet("{id}")]
@@ -32,5 +48,14 @@ namespace API.Controllers
       return product;
     }
 
+    [HttpGet("filters")]
+    public async Task<IActionResult> GetFilter()
+    {
+      var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+
+      var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+      return Ok(new { brands, types });
+    }
   }
 }
